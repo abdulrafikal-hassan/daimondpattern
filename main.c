1ad9f514095fa8246f6a0df02bc57acb286b296b
babyabdul <aral-hassan@st.ug.edu.gh>
2019-03-31 03:46:44 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main(){
+    int num,r,c,sp;
+    printf("enter number of rows: ");
+    scanf("%d", &num);
+    for(r=1; r<=num; r++){
+        for(sp=num-r; sp>=1; sp--)
+            printf(" ");
+        for(c=1; c<=r; c++)
+            printf("%d",c);
+        for(c=r-1; c>=1; c--)
+            printf("%d",c);
+        printf("\n");
+
+    }
+    for(r=1; r<=num; r++){
+        for(sp=r; sp>=1; sp--)
+            printf(" ");
+        for(c=1; c<=(num-r); c++)
+            printf("%d", c);
+        for(c=num-r-1; c>=1; c--)
+            printf("%d",c);
+        printf("\n");
+    }
+}
